f6d2c55775225628a93da72188d57ac3b2cd1a1c
Talal Naveed <98701565+Talalnaveed786@users.noreply.github.com>
2024-06-21 23:49:51 +0500

    Day 1 - Completed
    Done making this project "Band - Name generator"

---
+++ Day-1/final-project.py
@@ -0,0 +1,6 @@
+print("Welcome to band name generator")
+
+city = input("What is the name of the city you grew in?\n" )
+pet = input("What is your pet's name?\n" )
+
+print(city + " " + pet )
---
+++ Day-1/input.py
@@ -0,0 +1,21 @@
+# name = input("What is your name?")
+# print(len(name))
+
+# -----------------------
+
+# Temporary variable
+
+# a =input("a : ")
+# b =input("b : ")
+
+# c = a
+# a = b
+# b = c
+
+# print("a : " + a)
+# print("b : " + b)
+
+# -----------------------
+
+
+
